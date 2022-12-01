7956ece4740722c81a5ccfba19dc5e5d27822313
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-12-01 17:32:13 +0900

    データ追加テスト(Vue.jsから)

--- www/script/vuejs.js
+++ www/script/vuejs.js
@@ -4,10 +4,36 @@ new Vue ({
         return{
             todos: [],
             text: '',
+            apiKey: "dee51657f9ee6f0b90fff0b1a9fa69dd557a23475814d9b9eadc7154226e41a8",
+            clientKey: "8b9cbea8e8391c33cb9bd74f4177a177df679ed4d3483ccb64ac6f0ac52e1319"
         };
     },
 
-    methods: {//メソッドを３つ定義している
+    methods: {
+        setData(){
+            var ncmb = new NCMB(this.apiKey, this.clientKey);
+
+            // NCMB.Objectのサブクラスを生成
+            var GameScore = ncmb.DataStore("GameScore");
+
+            // クラスの新しいインスタンスを生成
+            var gameScore = new GameScore();
+
+            var GameScore = ncmb.DataStore("GameScore");
+            var gameScore = new GameScore();
+
+            gameScore.set("score", 1337)
+                .set("playerName", "Taro")
+                .set("cheatMode", false)
+                .save()
+                .then(function(gameScore){
+                    // 保存後の処理
+                })
+                .catch(function(err){
+                    // エラー処理
+                });
+        },
+
         inputText(e){
             this.text = e.target.value;
         },
