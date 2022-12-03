479cc10bb97c4ef3e25ae68d8a7cb658846047ff
hrt4423 <106893174+hrt4423@users.noreply.github.com>
2022-12-03 10:20:25 +0900

    数値型で保存されない問題を修正

--- www/script/vuejs.js
+++ www/script/vuejs.js
@@ -3,8 +3,8 @@ new Vue ({
     data(){
         return{
             todos: [],
-            text: 0,
-            score: '',
+            text: '',
+            score: 0,
             apiKey: "dee51657f9ee6f0b90fff0b1a9fa69dd557a23475814d9b9eadc7154226e41a8",
             clientKey: "8b9cbea8e8391c33cb9bd74f4177a177df679ed4d3483ccb64ac6f0ac52e1319"
         };
@@ -42,7 +42,7 @@ new Vue ({
         },
 
         inputScore(e){
-            this.score = e.target.value;
+            this.score = Number(e.target.value);
         },
 
         addTodo(){
