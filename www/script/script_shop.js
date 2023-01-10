9f9cb303c6552183db22fc0e8a143935d1105341
hanabonbon <hatakunani@gmail.com>
2023-01-10 11:16:53 +0900

    1/10更新

--- www/script/script_shop.js
+++ www/script/script_shop.js
@@ -1,4 +1,24 @@
 // This is a JavaScript file
+
+Vue.component('open-modal',{
+  template : `
+    <div id="overlay" v-on:click="clickEvent">
+        <div id="content"　 v-on:click="stopEvent">
+          <p><slot></slot></p>
+          <button v-on:click="clickEvent">close</button>
+        </div>
+    </div>
+    `,
+  methods :{
+    clickEvent: function(){
+      this.$emit('from-child')
+     },
+    stopEvent: function(){
+      event.stopPropagation()
+    }    
+  }
+})
+
 new Vue({
   el: '#app',
   data: {
